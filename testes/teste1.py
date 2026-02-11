4d4f4bc0ff151c5d31a6f8739e7bb2ee8ecefb2b
mubonassi <115490684+mubonassi@users.noreply.github.com>
2026-02-11 19:03:20 +0000

    Aula 1

---
+++ testes/teste1.py
@@ -0,0 +1,22 @@
+#Comandos Básicos - PT.1
+
+#Tipos de Informações (Fluxo da Informação - Quem recebe e quem envia)
+#Entrada: É o fluxo de informação de quando o computador recebe a informação
+#Saída: É o fluxo de informação de quando o computador envia uma informação
+
+#Função -> realiza/executa um comando dado ao código
+#Ex: função() -> função sem parametro
+#Ex: função(dado) -> função com parametro
+
+#Função Print -> Exibe uma informação/dado em texto no terminal
+#Ex: print("texto")
+print("Oi, me chamo Murilo Bonassi")
+
+#Tipos de Dados - Principais
+print("Oi eu sou um texto aleatório.") #Texto -> String
+print(89) #Numero Inteiro -> Int/Integer
+print(38.42) #Numero Real -> Float
+
+#Tipos de Dados - "Avançados"
+print(True) #Binário - Verdadeiro/Falso -> Boolean (True/False)
+print(2*10+3) #Lógico -> Trabalhar com operação ( + - * / )
